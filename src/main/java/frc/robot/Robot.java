86fe78318cdbd9dad412ccad13f9c16b25424755
purpleA <purpleA@joeframma.com>
2020-01-29 16:55:44 -0600

    color sensor tests

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,19 +1,31 @@
 package frc.robot;
+import edu.wpi.first.wpilibj.I2C;
 //git test
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.I2C.Port;
+import edu.wpi.first.wpilibj.util.Color;
+
 import com.ctre.phoenix.motorcontrol.can.*;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import com.revrobotics.ColorSensorV3;
 
 public class Robot extends TimedRobot {
+    
+    I2C.Port i2Color = I2C.Port.kOnboard;
+
     CANSparkMax upper = new CANSparkMax(3, MotorType.kBrushless);
     CANSparkMax lower = new CANSparkMax(1, MotorType.kBrushless);
+
+    ColorSensorV3 scan = new ColorSensorV3(i2Color);
 
     WPI_TalonSRX tipper = new WPI_TalonSRX(9);
 
@@ -21,7 +33,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
-    
 
   }
 
@@ -45,6 +56,10 @@ public class Robot extends TimedRobot {
   
   @Override
   public void teleopPeriodic() {
+    System.out.println(scan.getRed());
+    System.out.println(scan.getBlue());
+    System.out.println(scan.getGreen());
+
     double powerOut = (-logi.getRawAxis(2) + 1)* 0.5;
     
     upper.set(powerOut);
