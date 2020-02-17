c2908fa1d54fec8f7c00ad9c43edc9da5ec1e185
WaconiaRobocats4198 <47279266+WaconiaRobocats4198@users.noreply.github.com>
2020-02-17 13:36:56 -0600

    hola

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,7 @@
 package frc.robot;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.AnalogInput;
+
 import edu.wpi.first.wpilibj.I2C;
 //git test
 import edu.wpi.first.wpilibj.Joystick;
@@ -12,6 +14,12 @@ import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.util.Color;
 
+import edu.wpi.first.networktables.*;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.shuffleboard.*;
+
 import com.ctre.phoenix.motorcontrol.can.*;
 
 import com.revrobotics.CANSparkMax;
@@ -21,23 +29,28 @@ import com.revrobotics.SparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.ControlType;
 
+
 import com.revrobotics.ColorSensorV3;
 
 public class Robot extends TimedRobot {
     
     // I2C.Port i2Color = I2C.Port.kOnboard;
 
-    CANSparkMax upper = new CANSparkMax(1, MotorType.kBrushless);
+    CANSparkMax inUse = new CANSparkMax(5, MotorType.kBrushless);
     // CANSparkMax lower = new CANSparkMax(1, MotorType.kBrushless);
 
-    CANEncoder upperEnc = new CANEncoder(upper);
+    CANEncoder upperEnc = new CANEncoder(inUse);
     // CANEncoder lowerEnc = new CANEncoder(lower);
 
-    CANPIDController uSpeedControl = new CANPIDController(upper);
+    CANPIDController uSpeedControl = new CANPIDController(inUse);
     // CANPIDController lSpeedControl = new CANPIDController(lower);
 
     DigitalInput testSwitch = new DigitalInput(0);
+    private ShuffleboardTab tab = Shuffleboard.getTab("Auto");
 
+    // public NetworkTableEntry maxSpeed =
+    //       tab.add("Auto", 1)
+    //       .getEntry();
 
     // public static WPI_TalonSRX frontLeft = new WPI_TalonSRX(4);
     // public static WPI_TalonSRX frontRight = new WPI_TalonSRX(5);
@@ -53,6 +66,9 @@ public class Robot extends TimedRobot {
     Joystick logi = new Joystick(1);
 
     limelight tracker = new limelight();
+
+    public static DigitalInput inSensor = new DigitalInput(0);
+    public static DigitalInput outSensor = new DigitalInput(1);
 
     double kP = 1e-4; 
     double kI = 3e-7;
@@ -84,6 +100,9 @@ public class Robot extends TimedRobot {
     // lSpeedControl.setIZone(kIz);
     // lSpeedControl.setFF(kFF);
     // lSpeedControl.setOutputRange(kMinOutput, kMaxOutput);
+    
+
+
   }
 
  
@@ -121,7 +140,19 @@ public class Robot extends TimedRobot {
     // System.out.println(colorTest.colorOutput(blue, green, red));
     
 
-    // double powerOut = (-logi.getRawAxis(2) + 1) * 0.5;
+    double powerOut = (-logi.getRawAxis(2) + 1) * 0.5;
+    int multiply;
+    if(logi.getRawButton(6)){
+      multiply = 1;
+    }
+    else if(logi.getRawButton(7)){
+      multiply = -1;
+    }
+    else{
+      multiply = 0;
+    }
+    inUse.set(powerOut * multiply);
+
     // int powerRead = (int)(powerOut*100);
 
     // System.out.println(powerRead);
@@ -139,13 +170,15 @@ public class Robot extends TimedRobot {
     // }
 
     // uSpeedControl.setReference(-5400, ControlType.kVelocity);
-    if(logi.getRawButton(1)){
-      upper.set(0.5);
+    // if(logi.getRawButton(1)){
+    //   upper.set(0.3);
 
-    }
-    else{
-      upper.set(0);
-    }
+    // }
+    // else{
+    //   upper.set(0);
+    // }
+
+    // System.out.println(outSensor.get());
 
     
     // if(logi.getRawButtonReleased(3)){
